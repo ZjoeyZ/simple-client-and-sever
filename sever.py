62c4bc09e573cb656de72ecb5dc237fa2381986f
ZjoeyZ <1025623345@qq.con>
2019-05-05 22:37:13 +0800

    another server more complete

--- sever.py
+++ sever.py
@@ -1,21 +1,22 @@
+# 功能： 绑定主机2000端口，接收访问请求，返回简单响应并打印
 import socket
 
 s = socket.socket()
 
 host = ''
 port = 2000
-s.bind((host, port))  #服务器绑定端口
+s.bind((host, port))  # 服务器绑定端口
 
 while True:
 
     s.listen(5)
-    
-    connection, address = s.accept()#recive socket 对象 和地址
+
+    connection, address = s.accept()  # recive socket 对象 和地址
 
     buffer_size = 500
     request_byte = b''
 
-    #recieve all the byte
+    # recieve all the byte
     while True:
         request = connection.recv(buffer_size)
         request_byte += request
@@ -25,41 +26,7 @@ while True:
     print('requester\'s ip port and request, {}\n{}'.format(address, request_byte.decode('utf-8')))
 
     response = b'HTTP/1.1 200 OK\r\n\r\n<h1>Hello World!</h1>'
-    
+
     connection.sendall(response)
 
     connection.close()
-
-# 1,浏览器访问 http://localhost:2000/
-
-# 2,服务器第一次返回
-# requester's ip port and request, ('127.0.0.1', 1035)
-# GET / HTTP/1.1
-# Host: localhost:2000 #说明浏览器请求的是谁
-# .......
-
-# 2，服务器第二次返回
-# GET /favicon.ico HTTP/1.1
-# Host: localhost:2000
-
-#User-Agent:
-    # Mozilla/5.0 (Windows NT 10.0; Win64; x64)
-    # AppleWebKit/537.36 (KHTML, like Gecko) 苹果内核
-    # Chrome/73.0.3683.103 我是谷歌，我的版本是
-    # Safari/537.36
-
-# User-Agent:
-    # Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:66.0)
-    # Gecko/20100101    火狐内核
-    # Firefox/66.0  我是火狐，我的版本是
-
-
-# q=0.8
-# Accept: image/webp,*/*（任意对象任意类型）
-    # WebP格式，谷歌（google）开发的一种旨在加快图片加载速度的图片格式。
-    # 图片压缩体积大约只有JPEG的2/3，并能节省大量的服务器宽带资源和数据空间。
-# Accept-Language: zh-CN,zh;q=0.8（权重，优先级）,zh-TW;q=0.7,zh-HK;q=0.5,en-US;q=0.3,en;q=0.2
-# Accept-Encoding: gzip, deflate算法（压缩格式）
-# Accept-Language: zh-CN,zh（中文-大陆）;q=0.8,zh-TW（中文-台湾）;q=0.7,zh-HK（中文-香港）;q=0.5,en-US;q=0.3,en;q=0.2
-# Connection: keep-alive #保持connection不中断，便于请求多次，比如图片等
-# Cache-Control: max-age=0
---
+++ sever2.py
@@ -0,0 +1,96 @@
+# 功能：绑定主机3000端口，接收访问请求，解析，根据请求路径不同返回不同响应
+import socket
+
+
+def log(*args, **kwargs):
+    """
+    用这个 log 替代 print
+    """
+    print('log', *args, **kwargs)
+
+
+def route_index():
+    """
+    主页的处理函数, 返回主页的响应
+    """
+    header = 'HTTP/1.1 200 OK\r\nContent-Type: text/html\r\n'
+    body = '<h1>Hello World</h1><img src="doge.gif"/>'
+    r = header + '\r\n' + body
+    return r.encode(encoding='utf-8')
+
+
+def route_image():
+    """
+    图片的处理函数, 浏览器img标签会发出这个请求，读取图片并生成响应返回
+    """
+    with open('doge.gif', 'rb') as f:
+        header = b'HTTP/1.x 200 OK\r\nContent-Type: image/gif\r\n\r\n'
+        img = header + f.read()
+        return img
+
+
+# route_path_not_exsit()
+def error(code=404):
+    """
+    根据 code 返回不同的错误响应
+    目前只有 404
+    """
+    # 之前上课我说过不要用数字来作为字典的 key
+    # 但是在 HTTP 协议中 code 都是数字似乎更方便所以打破了这个原则
+    e = {
+        404: b'HTTP/1.x 404 NOT FOUND\r\n\r\n<h1>NOT FOUND</h1>',
+    }
+    return e.get(code, b'')
+
+
+def response_for_path(path):
+    """
+    根据 path 调用相应的处理函数
+    没有处理的 path 会返回 404
+    """
+    r = {
+        '/': route_index,
+        '/doge.gif': route_image,
+    }
+    response = r.get(path, error)
+    return response()
+
+
+def run(host='', port=3000):
+    """
+    启动服务器
+    """
+    # 初始化 socket 套路
+    # 使用 with 可以保证程序中断的时候正确关闭 socket 释放占用的端口
+    with socket.socket() as s:
+        s.bind((host, port))
+        # 无限循环来处理请求
+        while True:
+            # 监听 接受 读取请求数据 解码成字符串
+            s.listen(5)
+            connection, address = s.accept()
+            request = connection.recv(1024)
+            request = request.decode('utf-8')
+            log('ip and request, {}\n{}'.format(address, request))
+            try:
+                # 因为 chrome 会发送空请求导致 split 得到空 list
+                # 所以这里用 try 防止程序崩溃
+                path = request.split()[1]
+                # 用 response_for_path 函数来得到 path 对应的响应内容
+                response = response_for_path(path)
+                # 把响应发送给客户端
+                connection.sendall(response)
+            except Exception as e:
+                log('error', e)
+            # 处理完请求, 关闭连接
+            connection.close()
+
+
+if __name__ == '__main__':
+    # 生成配置并且运行程序
+    config = dict(
+        host='',
+        port=3000,
+    )
+    # 字典解包，传参 key = word
+    run(**config)
